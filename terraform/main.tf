3d1327572c0dff9689202349e7414d99ceb1d144
Gayan <gayanshaminda2001@gmail.com>
2025-03-30 14:11:35 +0530

    changes1

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,26 +1,51 @@
-
 provider "aws" {
-  region = "eu-north-1"  
+  region = "eu-north-1"
 }
 
-# EC2 instance resource definition
+resource "aws_security_group" "ecommerce_sg" {
+  name        = "E-commerce-security-group-v2"
+  description = "Security group for E-Commerce app"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+
+  ingress {
+    from_port   = 5173
+    to_port     = 5173
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-09a9858973b288bdd"  
-  instance_type = "t3.micro"  
+  ami           = "ami-09a9858973b288bdd"
+  instance_type = "t3.micro"
+  key_name      = "E-commerece-key-pair"
+  security_groups = [aws_security_group.ecommerce_sg.name]
 
-  
-  key_name = "E-commerece-key-pair"  
-
-  # Security group for the instance
-  security_groups = ["E-commerce-security-group"]  
-
- 
   tags = {
     Name = "MyEC2Instance"
   }
 }
 
-# Output the public IP of the instance
-output "public_ip" {
-  value = aws_instance.my_ec2.public_ip
+output "ssh_command" {
+  value = "ssh -i ~/.ssh/E-commerece-key-pair.pem ubuntu@${aws_instance.my_ec2.public_ip}"
 }
