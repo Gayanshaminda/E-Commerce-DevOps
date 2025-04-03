300742e066234209e72889f8e0deccbc62747b98
Gayan <gayanshaminda2001@gmail.com>
2025-04-03 18:41:36 +0530

    full updated vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
@@ -8,20 +8,18 @@ export default defineConfig({
         port: 5173,
         proxy: {
             "/api": {
-                target: "http://backend_container:5000", 
+                target: "https://e607-203-189-184-48.ngrok-free.app", // Use your ngrok public URL
                 changeOrigin: true,
-                secure: false,
-                rewrite: (path) => path
+                secure: false, // Disable SSL verification if using HTTP
+                rewrite: (path) => path // Keep the original path
             }
         }
     },
     preview: {
         host: '0.0.0.0',
-        port: 5173,
-        allowedHosts: ['e607-203-189-184-48.ngrok-free.app']
-        
+        port: 5173
     },
     build: {
-        sourcemap: true 
+        sourcemap: true
     }
-})
+});
